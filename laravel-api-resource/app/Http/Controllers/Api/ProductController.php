9bd1a43a9663bda077d93ac0148336342a5d0484
ildapskuy26 <adiahmaddd398@gmail.com>
2025-04-11 11:09:37 +0700

    praktikum12

---
+++ laravel-api-resource/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted']);
+    }
+}
